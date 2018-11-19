0943fa81adb35bf5362d8605408b3409a90a9263
jjesuscastro <joesei_castro@dlsu.edu.ph>
2018-11-20 01:03:14 +0800

    added fade from black and moved down  mc

---
+++ Assets/Scripts/StartFade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFade : MonoBehaviour {
+
+	public float rateOfFade;
+	SpriteRenderer sRenderer;
+
+	// Use this for initialization
+	void Start () {
+		sRenderer = gameObject.GetComponent<SpriteRenderer>();
+		Color tmpColor = sRenderer.color;
+		tmpColor.a = 1;
+		sRenderer.color = tmpColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color tmpColor = sRenderer.color;
+		tmpColor.a -= rateOfFade;
+		sRenderer.color = tmpColor;
+	}
+}
