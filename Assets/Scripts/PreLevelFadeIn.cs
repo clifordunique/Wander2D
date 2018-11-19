ac178adbafd1ee52e0e9fe2af761b6144c858d95
jjesuscastro <joesei_castro@dlsu.edu.ph>
2018-11-20 04:26:54 +0800

    scene1_polish
    added animation for environment
    
    papers disappear when interacted with
    
    changed assets

---
+++ Assets/Scripts/PreLevelFadeIn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreLevelFadeIn : MonoBehaviour {
+
+	public float fadeRate;
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		Color tempColor = spriteRenderer.color;
+		tempColor.a = 1;
+		spriteRenderer.color = tempColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color tempColor = spriteRenderer.color;
+		tempColor.a -= fadeRate;
+		spriteRenderer.color = tempColor;
+	}
+}
