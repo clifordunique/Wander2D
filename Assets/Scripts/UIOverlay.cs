9868bf342dd8ee76094eccd39dc8e20839d6a6f9
jjesuscastro <joesei_castro@dlsu.edu.ph>
2018-11-20 20:22:40 +0800

    journal and backpack button
    journal and backpack buttons should now open an overlay (a title for backpack) and a close button that adjusts to the screen

--- Assets/Scripts/UIOverlay.cs
+++ Assets/Scripts/UIOverlay.cs
@@ -2,31 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PreLevelFadeIn : MonoBehaviour {
+public class UIOverlay : MonoBehaviour {
 
-	public float fadeRate;
+	public float alphaValue;
 	SpriteRenderer sRenderer;
 
 	// Use this for initialization
 	void Start () {
-		sRenderer = GetComponent<SpriteRenderer>();
-		Color tempColor = sRenderer.color;
+		sRenderer = GetComponent<SpriteRenderer>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ScaleToScreen();
 
+		Color tempColor = sRenderer.color;
+		tempColor.a = alphaValue;
+		sRenderer.color = tempColor;
+	}
+
+	void ScaleToScreen() {
 		float width = sRenderer.sprite.bounds.size.x;
 		float height = sRenderer.sprite.bounds.size.y;
 		float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
 		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
 
 		transform.localScale = new Vector2 (worldScreenWidth / width, worldScreenHeight / height);
-
-		tempColor.a = 1;
-		sRenderer.color = tempColor;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Color tempColor = sRenderer.color;
-		tempColor.a -= fadeRate;
-		sRenderer.color = tempColor;
 	}
 }
