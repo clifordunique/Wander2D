a42321c6e8f6c18cda3f269d7aeed76e0126f156
jjesuscastro <joesei_castro@dlsu.edu.ph>
2018-11-21 17:02:51 +0800

    updated assets scene 4 and 5
    SCENE 4 PROBLEM:
    YUNG BAR

---
+++ Assets/Scripts/ToggleTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTimer : MonoBehaviour {
+
+    [SerializeField]
+    GameObject gObject;
+    public float duration;
+    float effectTimer;
+	
+	// Update is called once per frame
+	void Update () {
+        effectTimer += Time.deltaTime;
+        if(effectTimer % 60 < duration)
+        {
+            if (gObject.active)
+            {
+                gObject.SetActive(false);
+                effectTimer = 0;
+            }
+            else
+            {
+                gObject.SetActive(true);
+                effectTimer = 0;
+            }
+        }
+	}
+}
