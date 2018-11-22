9a9b18607259194e1f797e3a14ed36c9e870e8c4
carlacresencio <carla_cresencio@dlsu.edu.ph>
2018-11-23 01:02:38 +0800

    footsteps

--- Assets/Scripts/InteractJournalEntry.cs
+++ Assets/Scripts/InteractJournalEntry.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class InteractJournalEntry : MonoBehaviour {
 
+
+	public AudioSource paper;
+
 	// Use this for initialization
 	void Start () {
-		
+		paper = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	public void Interact()
 	{
 		gameObject.SetActive(false);
+		paper.Play();
 	}
 }
